2050536830d0a1c3aa0c7e6f48f58001307026e6
Cris0133 <143001978+Cris0133@users.noreply.github.com>
2023-08-29 18:29:04 -0500

    Add files via upload

---
+++ PrimerosProgramas/BakeSale.java
@@ -0,0 +1,14 @@
+
+package bakesale;
+
+public class BakeSale {
+
+    public static void main(String[] args) {
+        int numCakes = 33;
+        numCakes -= 10;
+    System.out.println (numCakes);
+        numCakes /= 3;
+    System.out.println (numCakes);
+    }
+    
+}
