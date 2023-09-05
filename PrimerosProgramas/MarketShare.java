db26fac27f0e2a87f272392ab4b723b1f780448a
Cris0133 <143001978+Cris0133@users.noreply.github.com>
2023-09-04 21:55:27 -0500

    Add files via upload

---
+++ PrimerosProgramas/MarketShare.java
@@ -0,0 +1,10 @@
+package marketshare;
+
+public class MarketShare {
+
+    public static void main(String[] args) {
+        double AppleShare = 33.01;
+        System.out.println (AppleShare);
+    }
+    
+}
